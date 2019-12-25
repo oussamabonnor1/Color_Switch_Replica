408ad508431b69129b55bc1dc3141f79f396bd5c
oussamabonnor1 <oussamabonnor@gmail.com>
2019-12-25 22:06:39 +0100

    adding project files

---
+++ Assets/Scripts/CircleBehaviour.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CircleBehaviour : MonoBehaviour
+{
+    public float rotationSpeed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, 0, rotationSpeed * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour
+{
+    public int jumpingForce;
+    public Rigidbody2D rb;
+    public GameObject destroyedPrefab;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.velocity = Vector2.up * jumpingForce;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "PowerUp")
+        {
+
+        }
+        else
+        {
+            if (other.gameObject.tag != gameObject.tag)
+            {
+                Instantiate(destroyedPrefab, transform.position, Quaternion.identity);
+                Destroy(gameObject);
+            }
+        }
+    }
+}
